1c5b136a59868a4e2afed8036909ab5a43e7a527
Gareth Grindeland <gt.grindeland@gmail.com>
2023-03-24 09:17:54 -0700

    Add UI logic to handle gathering text inputs from the form for the user's name and email.

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,15 @@ function handleRadio(event)  {
   const radioSelection = document.querySelector("input[name='use']:checked").value;
   console.log(radioSelection);
 }
+function handleText(event) {
+  event.preventDefault();
+  const userName = document.getElementById("Input1").value;
+  const userEmail = document.getElementById("Input2").value;
+  console.log(userName);
+  console.log(userEmail);
+}
 
 window.addEventListener("load", function()  {
   document.getElementById("user-form").addEventListener("submit", handleRadio);
+  document.getElementById("user-form").addEventListener("submit", handleText);
 });
