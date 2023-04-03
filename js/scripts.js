bb05aaefa833702fead76822ceb9551591d6fa81
Gareth Grindeland <gt.grindeland@gmail.com>
2023-04-02 19:30:28 -0700

    Remove logic containing the unused variables. Changed last 'else if' branch to 'else'.

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,7 @@ function handleRadio(event) {
   } else if (radioSelection === "career") {
     let cOutput = document.getElementById("c");
     cOutput.removeAttribute("class");
-  } else if (radioSelection === "other") {
+  } else  {
     let generalResources = document.getElementById("general-resources");
     generalResources.removeAttribute("class");
   }
@@ -26,23 +26,12 @@ function handleRadio(event) {
 function handleText(event) {
   event.preventDefault();
   const userName = document.getElementById("Input1").value;
-  const userEmail = document.getElementById("Input2").value;
   let greeting = document.getElementById("greeting");
   greeting.removeAttribute("class");
   document.getElementById("name-output").innerText = userName;
-}
-function handlePronouns(event) {
-  event.preventDefault();
-  const pronouns = document.getElementById("pronouns").value;
-}
-function handleAge(event) {
-  event.preventDefault();
-  const dob = document.getElementById("born").value;
 }
 
 window.addEventListener("load", function() {
   document.getElementById("user-form").addEventListener("submit", handleRadio);
   document.getElementById("user-form").addEventListener("submit", handleText);
-  document.getElementById("user-form").addEventListener("submit", handleAge);
-  document.getElementById("user-form").addEventListener("submit", handlePronouns);
 });
